fd1ac7c3f35217c633dde13fe0e76acbeebdc5cb
Vinícius Sortgetz Alves <140643334+ViniciusSorgetz@users.noreply.github.com>
2025-03-02 21:05:24 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto feito pelo https://curso.dev para treinar programação e me sentir um programar genuinamente competente
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-  return <h1>Oliver Tree vai lançar um novo album! Love you madly hate you badly</h1>
+function Home() {
+  return (
+    <h1>Oliver Tree vai lançar um novo album! Love you madly hate you badly</h1>
+  );
 }
 
-export default Home; 
+function teste() {
+  console.log("OPA!!!!!");
+}
+
+export default Home;
