4a4ea1f001d561a83445400f32490c2d41aa30d7
Vinícius Sortgetz Alves <140643334+ViniciusSorgetz@users.noreply.github.com>
2025-03-08 14:31:31 +0000

    Makes the `/status` route return the updated_at, max_connections, connections and postgres_version properties

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,18 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SELECT version() AS version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const connections = await database.query(
+    "SELECT * FROM pg_stat_activity AS connections;",
+  );
+
   res.status(200).json({
-    message: "Alunos do curso.dev são pessoas acima da média!",
+    postgres_version: postgresVersion.rows[0].version,
+    max_connections: maxConnections.rows[0].max_connections,
+    connections: connections.rowCount,
+    updated_at: updatedAt,
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("GET to /api/v1/status shoudl return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.postgres_version).toBeDefined();
+  expect(typeof responseBody.postgres_version).toEqual("string");
+
+  expect(responseBody.max_connections).toBeDefined();
+  const maxConnectionsNumber = Number(responseBody.max_connections);
+  expect(String(maxConnectionsNumber)).toEqual(responseBody.max_connections);
+
+  expect(responseBody.connections).toBeDefined();
+  expect(typeof responseBody.connections).toEqual("number");
+
+  console.log(responseBody);
 });
