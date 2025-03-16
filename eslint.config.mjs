29919c70a15b421107adc656bd7de8e67f1b2cc9
Vinícius Sortgetz Alves <140643334+ViniciusSorgetz@users.noreply.github.com>
2025-03-16 16:58:20 +0000

    Adds `eslint` in the project, with the file `eslint.config.mjs`. Some code corrections are made in some files

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import { globalIgnores } from "eslint/config";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-undef": "off",
+    },
+  },
+  globalIgnores([".next/", "pages/index.js"]),
+];
