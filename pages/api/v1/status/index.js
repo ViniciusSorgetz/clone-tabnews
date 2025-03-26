b8268e8c768e8da7124fee479a57533c4eab2097
Vinícius Sortgetz Alves <140643334+ViniciusSorgetz@users.noreply.github.com>
2025-03-26 20:18:21 +0000

    feat: Add `next-connect` and `MethodNotAllowedError` to `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,37 +1,50 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
 
-async function status(req, res) {
-  try {
-    const databaseName = process.env.POSTGRES_DB;
-    const updatedAt = new Date().toISOString();
-    const postgresVersion = await database.query("SHOW server_version;");
-    const maxConnections = await database.query("SHOW max_connections;");
-    const connections = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity AS connections WHERE datname = $1;",
-      values: [databaseName],
-    });
+const router = createRouter();
 
-    res.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: postgresVersion.rows[0].server_version,
-          max_connections: parseInt(maxConnections.rows[0].max_connections),
-          connections: connections.rows[0].count,
-        },
-      },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
+router.get(getHandler);
 
-    console.log("\n Erro dentro do catch do controller");
-    console.error(publicErrorObject);
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
 
-    res.status(500).json(publicErrorObject);
-  }
+function onErrorHandler(error, req, res) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+
+  console.log("\n Erro dentro do catch do next-connect");
+  console.error(publicErrorObject);
+
+  res.status(500).json(publicErrorObject);
 }
 
-export default status;
+function onNoMatchHandler(req, res) {
+  const publicErrorObject = new MethodNotAllowedError();
+  res.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+async function getHandler(req, res) {
+  const databaseName = process.env.POSTGRES_DB;
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const connections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity AS connections WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        connections: connections.rows[0].count,
+      },
+    },
+  });
+}
