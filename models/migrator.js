ddc66bfd60f54d0e2c8c8ef2b20f370977eabdf3
Vinícius Sortgetz Alves <140643334+ViniciusSorgetz@users.noreply.github.com>
2025-03-27 13:51:34 +0000

    feat: Create model `migrator.js` and implement it in `migrations` endpoint

---
+++ models/migrator.js
@@ -0,0 +1,63 @@
+import database from "infra/database";
+import { resolve } from "node:path";
+import migrationRunner from "node-pg-migrate";
+import { ServiceError } from "infra/errors";
+
+const defaultMigrationOptions = {
+  databaseUrl: process.env.DATABASE_URL,
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pandingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+    return pandingMigrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      cause: error,
+      message: "Erro ao rodar migrações no banco de dados.",
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pandingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient,
+    });
+    return pandingMigrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      cause: error,
+      message: "Erro ao rodar migrações no banco de dados.",
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
