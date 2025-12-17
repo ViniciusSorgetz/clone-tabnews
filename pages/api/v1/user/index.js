c6746997ad4e2e77961d22fec1adf6dc80199b33
Vinícius Sortgetz Alves <viniciussorgetzalves@gmail.com>
2025-12-17 18:22:20 +0000

    feat: Implement system to `renew` a session in `/user/`

---
+++ pages/api/v1/user/index.js
@@ -0,0 +1,22 @@
+import controller from "infra/controller";
+import { createRouter } from "next-connect";
+import user from "models/user.js";
+import session from "models/session";
+
+const router = createRouter();
+
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(req, res) {
+  const sessionToken = req.cookies.session_id;
+
+  const foundSession = await session.findValidOneByToken(sessionToken);
+  const renewedSession = await session.renew(foundSession.id);
+  controller.setSessionCookie(renewedSession.token, res);
+
+  const foundUser = await user.findOneById(foundSession.user_id);
+
+  res.status(200).json(foundUser);
+}
