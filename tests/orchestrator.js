97cb8ce2ea9fb8d98bb1c50e48cf7595cf2be1ce
Vinícius Sortgetz Alves <viniciussorgetzalves@gmail.com>
2026-03-31 01:10:51 +0000

    feat: Add `email.js` with `nodemailer` at infra folder

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,8 +6,25 @@ import migrator from "models/migrator.js";
 import user from "models/user.js";
 import session from "models/session";
 
+const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
+
+  async function waitForEmailServer() {
+    return retry(fetchEmailPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchEmailPage() {
+      const response = await fetch(emailHttpUrl);
+      if (response.status !== 200) {
+        throw Error();
+      }
+    }
+  }
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
@@ -46,12 +63,36 @@ async function createSession(userId) {
   return await session.create(userId);
 }
 
+async function deleteAllEmails() {
+  await fetch(`${emailHttpUrl}/messages`, {
+    method: "DELETE",
+  });
+}
+
+async function getLastEmail() {
+  const emailListResponse = await fetch(`${emailHttpUrl}/messages`);
+  const emailListBody = await emailListResponse.json();
+  const lastEmailItem = emailListBody.pop();
+
+  const emailTextResponse = await fetch(
+    `${emailHttpUrl}/messages/${lastEmailItem.id}.plain`,
+  );
+  const emailTextBody = await emailTextResponse.text();
+
+  return {
+    ...lastEmailItem,
+    text: emailTextBody,
+  };
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
   createUser,
   createSession,
+  deleteAllEmails,
+  getLastEmail,
 };
 
 export default orchestrator;
